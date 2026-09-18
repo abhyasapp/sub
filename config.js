a4f687eaf2b9f97c91a490fc1d2e62098888951c
abhyasapp <abhyasbymku@gmail.com>
2026-09-18 11:31:59 +0545

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,7 @@
    backend URL.
    ═══════════════════════════════════════════════════════════════════════ */
 const ABHYAS_CONFIG = Object.freeze({
-  GAS_URL: "https://script.google.com/macros/s/AKfycbyZLZxf6VKNiMfhY6memPTKj-dGW7jxyX1c-9GI0OPG8TqazSZi_P-7Y-8DlpT0ZlrjHg/exec"
+  GAS_URL: "https://script.google.com/macros/s/AKfycbyZLZxf6VKNiMfhY6memPTKj-dGW7jxyX1c-9GI0OPG8TqazSZi_P-7Y-8DlpT0ZlrjHg/exec",
+  GOOGLE_CLIENT_ID: "242226857075-hpkbjoqhlem95fu6vkf712e8ijs33sng.apps.googleusercontent.com"
 });
+window.ABHYAS_CONFIG = ABHYAS_CONFIG;
